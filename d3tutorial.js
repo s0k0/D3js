ff97c718d7c2e01275886d449e24e447ca10bc53
Sophie Kolbe <sophie.kolbe@zalando.de>
2016-10-27 14:20:15 +0200

    NPM and pie char directive - added direcrive to create pie charts - remove bower packages and replaced them with npm - added dress code (no scss yet, so only html styling :/ )

--- d3tutorial.js
+++ d3tutorial.js
@@ -9,27 +9,38 @@ app.controller("ChartController", ["$scope","$interval", function($scope) {
     };
 
     // create data
-    $scope.data = [];
-    var totalPoints = 0;
+    $scope.scatter = [];
+    var totalPoints = 50;
     for(var i = 0; i < totalPoints; i++){
-        $scope.data.push(randPoint());
+        $scope.scatter.push(randPoint());
     }
+
+    $scope.pie = [
+        {age: '<5' , population: 120},
+        {age: '5-13' , population: 260},
+        {age: '14-17' , population: 300},
+        {age: '28-24' , population:  250},
+        {age: '25-44' , population: 600},
+        {age: '45-64' ,population:  320},
+        {age: '>=65' , population: 150}
+    ];
+
 
 }]);
 
-app.directive("myChart", function($window) {
+app.directive("scatterPlot", function($window) {
     return{
         restrict: "E",
         controller: "ChartController",
         controllerAs: 'cc',
         bindToController: {
-            chartData: '=',
+            data: '=',
             width: '=',
             height: '='
         },
         link: function(scope, element, attrs, model){
             //create variables for settings
-            var dataToPlot = scope.cc.chartData;
+            var data = scope.cc.data;
             var width = scope.cc.width;
             var height = scope.cc.height;
             var margin = 60;
@@ -127,7 +138,82 @@ app.directive("myChart", function($window) {
 
             };
 
-            draw(svg,width, height, margin ,dataToPlot);
+            draw(svg,width, height, margin ,data);
+        }
+    };
+});
+
+
+app.directive("pieChart", function($window) {
+    return{
+        restrict: "E",
+        controller: "ChartController",
+        controllerAs: 'cc',
+        bindToController: {
+            data: '=',
+            width: '=',
+            height: '='
+        },
+        link: function(scope, element, attrs, model){
+            //create variables for svg settings
+            var data = scope.cc.data;
+            var width = scope.cc.width;
+            var height = scope.cc.height;
+            var radius = Math.min(width, height) /2;
+            var innerRadius = radius - 80;
+            var outerRadius = radius - 10;
+            var labelRadius = radius - 40;
+
+
+            //d3 settings
+            var d3 = $window.d3;
+            var color = d3.scaleOrdinal()
+                .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+            var arc = d3.arc()
+                .outerRadius(outerRadius)
+                .innerRadius(innerRadius);
+
+            var labelArc = d3.arc()
+                .outerRadius(labelRadius)
+                .innerRadius(labelRadius);
+
+            var pie = d3.pie()
+                .padAngle(.02)
+                .value(function(d) { return d.population; })
+                (data);
+
+
+            //create svg root element width given height and width
+            var svg = d3.select(element[0]).append('svg')
+                .attr('width', width)
+                .attr('height', height);
+
+            // create group for chart and append to root
+            svg.append('g')
+                .attr('class', 'data')
+                .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+
+            // draw function to create plot
+            var draw = function(svg, data){
+                // Add new the data points
+               var g =  svg.select('.data')
+                    .selectAll('.arc')
+                    .data(pie)
+                    .enter()
+                    .append('g')
+                    .attr('class', 'arc');
+
+                g.append("path")
+                    .attr("d", arc)
+                    .style("fill", function(d) { return color(d.data.age); });
+
+                g.append("text")
+                    .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+                    .attr("dy", ".35em")
+                    .text(function(d) { return d.data.age; });
+            };
+
+            draw(svg ,data);
         }
     };
 });
